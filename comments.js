8cef9097ee6f94a0ffc177f86f7eddb9b2301279
JVelas <jirka.velas@brezove-hory.cz>
2024-06-21 07:47:57 +0200

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,20 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file.
+// The comments.html file should be served with the correct content type.
+// The comments.html file should contain the following HTML:
+// <!DOCTYPE html>
+// <html>
+// <head>
+//   <title>Comments</title>
+// </head>
+// <body>
+//   <h1>Comments</h1>
+//   <ul id="comments">
+//   </ul>
+// </body>
+// </html>
+// Use the fs module to read the comments.html file.
+// Use the http module to create a server.
+// Use the res.writeHead() function to set the content type.
+// Use the res.end() function to send the response.
+// Use the listen() function to start the server.
